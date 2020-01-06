bc23da055b71d9360095717b9ef60c4c65e61ffe
vivek <vivek.a@knowledgecity.pro>
2020-01-06 18:09:04 +0530

    added search functionality

--- server.js
+++ server.js
@@ -4,15 +4,21 @@ require('dotenv').config();
 
 const express= require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+
 const mongoose = require("mongoose");
 app.set('view engine','ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit:'10mb', extended: false}));
+
 app.use('/' ,indexRouter);
+app.use('/authors' ,authorRouter)
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
 const db = mongoose.connection;
